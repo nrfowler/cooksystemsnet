13ffcda98f9101c150dd2b89540852ef7159ed34
nrfowler <nrfowler@gmail.com>
2017-04-10 16:38:23 -0500

    init: Word Dictionary Assignment
    For real this time, prior commit was mistake.

---
+++ DictWebApp/Default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DictWebApp
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var words = new NorthwindEntities().WordTables;
+            Dictionary<char, int> alphabet = new Dictionary<char, int>();
+            foreach( var word in words)
+            {
+                char firstLetter = word.Word.First();
+                if (alphabet.ContainsKey(firstLetter))
+                {
+                    int count = ++alphabet[firstLetter];
+                    alphabet.Remove(firstLetter);
+                    alphabet.Add(firstLetter, count);
+                }
+                else
+                    alphabet.Add(firstLetter, 1);
+
+                
+            }
+            dbTest.Text = "A : "+alphabet['a'].ToString()+ " X: " + alphabet['x'].ToString();
+
+        }
+    }
+}
---
+++ DictWebApp/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DictWebApp.Startup))]
+namespace DictWebApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
